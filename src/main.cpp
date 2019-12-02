de76db4d950dcb73f813dca80561cfe3a4d87113
Jason Chen <52260129+ilovehotcakes@users.noreply.github.com>
2019-12-01 23:47:00 -0800

    Main slight update

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,12 @@ int main(int argc, char* argv[]) {
 	VLAD kittens1("../sift/kittens1.hesaff.sift");
 	VLAD kittens2("../sift/kittens2.hesaff.sift");
 
-	//kittens1.drawVLAD();
-	//waitKey(0);
-	//kittens2.drawVLAD();
-
+	kittens1.drawVLAD();
+	waitKey(0);
+	kittens2.drawVLAD();
+	waitKey(0);
 	
-	kittens1.compareVLAD(kittens2);
+	//kittens1.compareVLAD(kittens2);
 
 
 	getchar();
