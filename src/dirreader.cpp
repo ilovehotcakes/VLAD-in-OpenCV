5fe3d7b706cf13a8eba2e7822777ad280a1a22a6
Jason Chen <52260129+ilovehotcakes@users.noreply.github.com>
2019-12-03 11:52:06 -0800

    Initial commit

---
+++ src/dirreader.cpp
@@ -0,0 +1,38 @@
+// This program outputs files in the specified directory to a .txt file
+#include <sys/types.h>
+#include <dirent.h>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void usage()
+{
+    
+}
+
+void read_directory(string name, vector<string> &v)
+{
+    DIR* dirp = opendir(name.c_str());
+    struct dirent * dp;
+    while ((dp = readdir(dirp)) != NULL) {
+        v.push_back(dp->d_name);
+    }
+    closedir(dirp);
+}
+
+
+int main(int argc, char* argv[]) {
+	vector<string> v;
+	read_directory(argv[1], v);
+
+	
+	ofstream output_file("./list.txt");
+    ostream_iterator<string> output_iterator(output_file, "\n");
+    copy(v.begin() + 2, v.end() - 10188, output_iterator);
+	cout << "There are " << v.size() - 2 << " files in this dir." << endl;
+
+	
+	return 0;
+}
