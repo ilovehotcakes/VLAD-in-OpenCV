5928bedcb7bb819f377721f7678bc38cb64cd698
Jason Chen <kidultimate@gmail.com>
2019-12-09 11:38:39 -0800

    Organizing files

--- buildhessift.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-cd src
-
-g++ $(pkg-config --cflags --libs /usr/local/Cellar/opencv/4.1.0_2/lib/pkgconfig/opencv4.pc) -std=c++11 -O3 -Wall -Werror -pedantic hesaff.cpp affine.cpp helpers.cpp pyramid.cpp siftdesc.cpp -o ../prog/hessift
-
-cd ..
